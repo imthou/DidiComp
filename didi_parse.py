333265f1f14c4a6290de57d7d93b09937fd118df
Tim Hou <thou5686@gmail.com>
2016-05-20 13:35:55 -0600

    updated didi parse for order info

--- didi_parse.py
+++ didi_parse.py
@@ -1,25 +1,31 @@
 import pandas as pd
 import sys
+import os
 
 class DidiParse(object):
     """
     Parses all Didi files
     """
 
-    def __init__(self, filename):
+    def __init__(self):
         """
-        Input: String
         """
-        self.filename = filename
+        self.orderpath = '/data/season_1/training_data/order_data/'
+        self.orderdir = os.listdir(os.getcwd() + self.orderpath)
         self.order_names = ['order_id','driver_id','passenger_id','start_district_hash','dest_district_hash','price','time']
+        self.order = []
         self._parse_order_info()
 
     def _parse_order_info(self):
         """
         Output: DataFrame
+
+        Parses all order info tables
         """
-        self.order = pd.read_csv(self.filename, delimiter='\t', names=self.order_names)
+        for filename in self.orderdir:
+            self.order.append(pd.read_csv(self.orderpath[1:] + filename, delimiter='\t', names=self.order_names, parse_dates=['time']))
+            print "parsed {}".format(filename)
+        self.order = pd.concat(self.order)
 
 if __name__ == '__main__':
-    filename = 'data/season_1/training_data/order_data/order_data_2016-01-01'
-    didi = DidiParse(filename=filename)
+    didi = DidiParse()
