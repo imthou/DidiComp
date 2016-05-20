12824457140babfa5b7e1ee249fe4150a03fd059
Tim Hou <thou5686@gmail.com>
2016-05-20 12:58:32 -0600

    cleaned parsing code

--- didi_parse.py
+++ didi_parse.py
@@ -2,21 +2,24 @@ import pandas as pd
 import sys
 
 class DidiParse(object):
+    """
+    Parses all Didi files
+    """
 
     def __init__(self, filename):
         """
         Input: String
         """
         self.filename = filename
+        self.order_names = ['order_id','driver_id','passenger_id','start_district_hash','dest_district_hash','price','time']
+        self._parse_order_info()
 
-    def parse_order_info(filename):
+    def _parse_order_info(self):
         """
-        
+        Output: DataFrame
         """
-        col_names = ['order_id','driver_id','passenger_id','start_district_hash','dest_district_hash','price','time']
-        df = pd.read_csv(filename, delimiter='\t', names=col_names)
+        self.order = pd.read_csv(self.filename, delimiter='\t', names=self.order_names)
 
 if __name__ == '__main__':
     filename = 'data/season_1/training_data/order_data/order_data_2016-01-01'
     didi = DidiParse(filename=filename)
-    parse_order_info(filename)
